b904ca978f5b55665efcc6f68b7414150a402149
René van den Berg <rjwvandenberg@gmail.com>
2017-04-28 14:32:01 +0200

    Moved files to new project structure.

--- plebnet/tests/test_localserver.py
+++ plebnet/tests/test_localserver.py
@@ -2,6 +2,9 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
+    def setup(self):
+        server = LocalhostBuyer()
+
     def test_something(self):
         self.assertEqual(True, False)
 
