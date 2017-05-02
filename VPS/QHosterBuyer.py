283867755b090b730af92550c9229f6b2a1730ff
Mitchell Hoppenbrouwer <mitchell.h.lang@gmail.com>
2017-05-02 12:30:43 +0200

    fixed error

--- VPS/QHosterBuyer.py
+++ VPS/QHosterBuyer.py
@@ -12,6 +12,7 @@ class QHosterBuyer(VPSBuyer):
         self.driver.find_element_by_css_selector('section.subplanbox > div:nth-of-type(1) > aside:nth-of-type(1) > div.orangebtn > a').click()
         self.driver.implicitly_wait(10)
         self.driver.find_element_by_css_selector('td.fieldarea > label:nth-of-type(15) > input').click()
+        self.driver.implicitly_wait(10)
         self.driver.find_element_by_css_selector('input.checkout').click()
         self.driver.implicitly_wait(10)
         self.fill_in_element('[name="firstname"]', self.generator.get_first_name())
@@ -20,6 +21,8 @@ class QHosterBuyer(VPSBuyer):
         self.fill_in_element('#newpw', self.generator.get_password())
         self.fill_in_element('tbody > tr:nth-of-type(6) > td:nth-of-type(2) > input', self.generator.get_password())
         self.fill_in_element('tbody > tr:first-child > td:nth-of-type(4) > input', self.generator.get_random_alphabetical_string(10))
+        # with open('image.png', 'wb') as f:
+        #    f.write(self.driver.get_screenshot_as_png())
         self.fill_in_element('[name="city"]', self.generator.get_city())
         self.fill_in_element('[name="postcode"]', self.generator.get_zipcode())
         self.choose_select_element('#country', "Netherlands")
@@ -28,6 +31,7 @@ class QHosterBuyer(VPSBuyer):
         self.driver.find_element_by_css_selector('#pgbtnbitpay').click()
         self.driver.find_element_by_css_selector('input.ordernow.submitbutton').click()
         self.driver.implicitly_wait(10)
+        self.driver.get_screenshot_as_png()
 
         self.driver.find_element_by_css_selector('#copy-tab > span').click()
         print(self.driver.find_element_by_css_selector('div.manual-box__amount__value.copy-cursor > span').text)
