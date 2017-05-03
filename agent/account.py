130313262b2bd0c51cfc98a62e6b30cd70302f5b
René van den Berg <rjwvandenberg@gmail.com>
2017-05-03 09:20:23 +0200

    Updated name of agent module.

---
+++ agent/account.py
@@ -0,0 +1,25 @@
+
+class Account(object):
+    '''
+    Account serves as a datastructure for all account details related to the agent.
+    '''
+
+    def __init__(self):
+
+        # Identity
+        self.first_name
+        self.last_name
+        self.email
+
+        # Whereabouts
+        self.city
+        self.state
+        self.postcode
+        self.phonenumber
+
+        # Login details
+        self.accountname
+        self.password
+
+        # Recovery options
+
