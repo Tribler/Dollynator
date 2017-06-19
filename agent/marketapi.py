4f1a4fc00ebd50dff9d64f03417b01266cd8c449
Jaap Heijligers <jaap@heijligers.me>
2017-06-19 11:15:32 +0200

    Implement market API

---
+++ agent/marketapi.py
@@ -0,0 +1,62 @@
+import requests
+from requests.exceptions import ConnectionError
+
+
+def is_market_running():
+    try:
+        requests.get('http://localhost:8085/market')
+        return True
+    except ConnectionError:
+        return False
+
+
+def get_balance():
+    r = requests.get('http://localhost:8085/wallets/MC/balance')
+    balance = r.json()
+    return balance['balance']['available']
+
+
+def put_ask(price, price_type, quantity, quantity_type):
+    return _put_request(price, price_type, quantity, quantity_type, 'asks')
+
+
+def put_bid(price, price_type, quantity, quantity_type):
+    return _put_request(price, price_type, quantity, quantity_type, 'bids')
+
+
+def _put_request(price, price_type, quantity, quantity_type, domain):
+    url = 'http://localhost:8085/market/' + domain
+    data = {'price': price,
+            'price_type': price_type,
+            'quantity': quantity,
+            'quantity_type': quantity_type}
+    json = requests.put(url, data=data).json()
+    if 'created' in json:
+        return json['created']
+    else:
+        print json['error']['message']
+        return False
+
+
+def asks():
+    url = 'http://localhost:8085/market/asks'
+    r = requests.get(url)
+    return r.json()['asks']
+
+
+def bids():
+    url = 'http://localhost:8085/market/bids'
+    r = requests.get(url)
+    return r.json()['bids']
+
+
+if __name__ == '__main__':
+    if not is_market_running():
+        print "Market isn't running"
+        exit(0)
+    print get_balance()
+    print put_bid(1, 'MC', 1, 'BTC')
+    print put_ask(1, 'MC', 1, 'BTC')
+    print asks()
+    print bids()
+    print is_market_running()
