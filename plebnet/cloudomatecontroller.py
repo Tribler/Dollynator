307e9b8ef4eeb2225f7781b71a3969b6962c7f22
Jaap Heijligers <jaap@heijligers.me>
2017-06-21 16:54:50 +0200

    Config generation bug fixes

--- plebnet/cloudomatecontroller.py
+++ plebnet/cloudomatecontroller.py
@@ -3,6 +3,7 @@ import ConfigParser
 import codecs
 import random
 
+import unicodedata
 from appdirs import user_config_dir
 from cloudomate.util.config import UserOptions, os
 from faker.factory import Factory
@@ -30,20 +31,29 @@ def purchase(provider, vps_option, wallet):
 
 def generate_config():
     config = UserOptions()
-    filename = os.path.join(user_config_dir(), 'test.cfg')
+    filename = os.path.join(user_config_dir(), 'cloudomate.cfg')
     if os.path.exists(filename):
         print("cloudomate.cfg already present at %s" % filename)
         config.read_settings(filename=filename)
         return config
-    locale = random.choice(['bg_BG', 'cs_CZ', 'de_DE', 'dk_DK', 'el_GR', 'es_ES', 'et_EE', 'hr_HR', 'it_IT', 'nl_NL'])
+    locale = random.choice(['bg_BG', 'cs_CZ', 'de_DE', 'dk_DK', 'es_ES', 'et_EE', 'hr_HR', 'it_IT'])
     print(locale)
     fake = Factory().create(locale)
     cp = ConfigParser.ConfigParser()
     _generate_address(cp, fake)
     _generate_server(cp, fake)
     _generate_user(cp, fake)
+    _remove_unicode(cp)
     with codecs.open(filename, 'w', 'utf8') as config_file:
         cp.write(config_file)
+
+
+def _remove_unicode(cp):
+    for section in cp.sections():
+        for option in cp.options(section):
+            item = cp.get(section, option)
+            if isinstance(item, unicode):
+                cp.set(section, option, unicodedata.normalize('NFKD', item).encode('ascii', 'ignore'))
 
 
 def _generate_user(cp, fake):
@@ -54,7 +64,7 @@ def _generate_user(cp, fake):
     cp.set('User', 'firstname', firstname)
     cp.set('User', 'lastname', lastname)
     cp.set('User', 'companyname', fake.company())
-    cp.set('User', 'phonenumber', fake.phone_number())
+    cp.set('User', 'phonenumber', fake.numerify('##########'))
     cp.set('User', 'password', fake.password())
 
 
