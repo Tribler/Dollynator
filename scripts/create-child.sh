b7b3483f1965c4718979baaa76b3694c96ac8dd5
Jaap Heijligers <jaap@heijligers.me>
2017-06-21 14:06:50 +0200

    Check for sshpass

--- scripts/create-child.sh
+++ scripts/create-child.sh
@@ -2,9 +2,10 @@
 IP=$1
 PASSWORD=$2
 
-apt-get install -y sshpass
+if ! hash sshpass 2> /dev/null; then
+    apt-get install -y sshpass
+fi
 
-echo "sshpass -p$PASSWORD ssh root@$IP echo hi"
 sshpass -p$PASSWORD ssh root@$IP 'apt-get update && \
     apt-get install git && \
     git clone https://github.com/rjwvandenberg/PlebNet && \
