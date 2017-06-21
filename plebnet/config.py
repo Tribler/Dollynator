2931cdb940c81342d45f8efd7d9467a9d45c1283
Jaap Heijligers <jaap@heijligers.me>
2017-06-21 18:21:11 +0200

    Use JSON instead of ConfigParser

--- plebnet/config.py
+++ plebnet/config.py
@@ -1,10 +1,10 @@
+import json
 import os
 import time
-from ConfigParser import SafeConfigParser
 
 from appdirs import user_config_dir
 
-CONFIG_NAME = "plebnet.cfg"
+CONFIG_NAME = "plebnet.json"
 
 
 class PlebNetConfig(object):
@@ -13,38 +13,39 @@ class PlebNetConfig(object):
         config_dir = user_config_dir()
         filename = os.path.join(config_dir, CONFIG_NAME)
         cfg = PlebNetConfig()
-        cfg.config.read(filename)
+        with open(filename, 'r') as json_file:
+            cfg.config = json.loads(json_file)
         return cfg
 
     def __init__(self):
-        self.config = SafeConfigParser()
-        config = self.config
-        config.add_section('main')
-        config.set('main', 'expiration_date', 0)
-        config.set('main', 'last_offer_date', 0)
-        config.set('main', 'last_offer', {'MC': 0, 'BTC:': 0.0})
-        config.set('main', 'excluded_providers', [])
-        config.set('main', 'chosen_providers', [])
-        config.set('main', 'bought', [])
+        config = self.config = {}
+        config['expiration_date'] = 0
+        config['last_offer_date'] = 0
+        config['last_offer'] = {'MC': 0, 'BTC:': 0.0}
+        config['excluded_providers'] = []
+        config['chosen_providers'] = []
+        config['bought'] = []
 
     def save(self):
         config_dir = user_config_dir()
         filename = os.path.join(config_dir, CONFIG_NAME)
+        print(self.config)
+        print(filename)
         with open(filename, 'w') as f:
-            self.config.write(f)
+            json.dump(self.config, f)
 
     def get(self, option):
-        return self.config.get('main', option)
+        return self.config[option]
 
     def time_to_expiration(self):
         current_time = time.time()
-        expiration = self.config.getfloat('main', 'expiration_date')
+        expiration = float(self.config['expiration_date'])
         return expiration - current_time
 
     def time_since_offer(self):
         current_time = time.time()
-        offer_time = self.config.getfloat('main', 'last_offer_date')
+        offer_time = float(self.config['last_offer_date'])
         return current_time - offer_time
 
     def set(self, option, value):
-        self.config.set('main', option, value)
+        self.config[option] = value
