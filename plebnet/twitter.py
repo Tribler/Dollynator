36d9877c8ed24d77a4be4544b325f52318ef057e
Jaap Heijligers <jaap@heijligers.me>
2017-06-29 16:01:03 +0200

    Error checking

--- plebnet/twitter.py
+++ plebnet/twitter.py
@@ -11,13 +11,24 @@ def tweet_arrival():
     options.read_settings()
     name = options.get('firstname') + ' ' + options.get('lastname')
 
+    path = os.path.join(user_config_dir(), 'twitter.cfg')
+    if not os.path.exists(path):
+        print("Can't Tweet: {0} doesn't exist".format(path))
+        return False
     cp = ConfigParser()
-    cp.read(os.path.join(user_config_dir(), 'twitter.cfg'))
+    cp.read(path)
 
-    twitter = Twython(cp.get('twitter', 'app_key'),
-                      cp.get('twitter', 'app_secret'),
-                      cp.get('twitter', 'oauth_token'),
-                      cp.get('twitter', 'oauth_token_secret'))
-    twitter.update_status(status='Pleb %s has joined the botnet for good.' % name)
+    try:
+        twitter = Twython(cp.get('twitter', 'app_key'),
+                          cp.get('twitter', 'app_secret'),
+                          cp.get('twitter', 'oauth_token'),
+                          cp.get('twitter', 'oauth_token_secret'))
+        twitter.update_status(status='Pleb %s has joined the botnet for good.' % name)
+        print("Tweeted arrival")
+    except Exception, e:
+        print e
+        return False
+    return True
+
 
 tweet_arrival()
