62517f7a69d28fef96e62ea2c0163ed868583166
Matouš Skála <skala.matous@gmail.com>
2018-11-28 01:32:41 +0100

    update repo url in install.sh

--- plebnet/clone/install.sh
+++ plebnet/clone/install.sh
@@ -132,7 +132,7 @@ then
     echo "default-keyring=keyrings.alt.file.PlaintextKeyring" >> $KRCFG
 fi
 
-[ ! -d "PlebNet" ] && git clone -b $BRANCH --recurse-submodules https://github.com/vwigmore/PlebNet
+[ ! -d "PlebNet" ] && git clone -b $BRANCH --recurse-submodules https://github.com/MattSkala/PlebNet
 
 # when branch is given, this create-child.sh's default branch value will be updated
 #   this is because the child's cloned repo also needs these values updated
