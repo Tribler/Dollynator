4db532baeeb0f892ec99c44139237b2afd3dd0fd
Giorgio Acquati <gioac96@gmail.com>
2020-05-19 12:42:21 +0200

    Implement node failures handling
    Co-authored-by: rgismondi <roberta.gismondi@gmail.com>

--- plebnet/contacts.py
+++ plebnet/contacts.py
@@ -3,9 +3,20 @@ import random
 import string
 import calendar
 import time
+import argparse
+import getpass
+import os
+
+from datetime import datetime
+from crontab import CronTab
 
 from plebnet.messaging import MessageSender
+from plebnet.messaging import MessageConsumer
 from plebnet.messaging import MessageReceiver
+
+def now():
+
+    return int(datetime.timestamp(datetime.now()))
 
 
 def generate_contact_id(parent_id: str = ""):
@@ -26,7 +37,6 @@ def generate_contact_id(parent_id: str = ""):
 
     return hash + timestamp
 
-
 class Contact:
     """
     Nodes contact.
@@ -37,11 +47,63 @@ class Contact:
     host: host of the node.
     port: message receiver listening port
     """
-    def __init__(self, id: str, host: str, port: int):
+    def __init__(self, id: str, host: str, port: int, first_failure = None):
         
         self.id = id
         self.host = host
         self.port = port
+
+        self.first_failure = None
+
+    def __get_ping_command(self, ab_port):
+
+        command = "PYTHONPATH=" + os.getcwd()
+        command += " python3 -m plebnet.contacts -p"
+        command += " --host " + self.host
+        command += " --port " + str(self.port)
+        command += " --ab-port " + str(ab_port)
+
+        return command
+
+
+    def __job_comment(self):
+
+        return "ping " + self.id + " " + self.host + " " + str(self.port)
+
+
+    def did_not_reply(self, ab_port, minutes_interval = 30):
+
+        if self.first_failure is None:
+
+            self.first_failure = now()
+
+        cron = CronTab(user=True)
+        job = cron.new(command = self.__get_ping_command(ab_port), comment=self.__job_comment())
+        job.minute.every(minutes_interval)
+
+        cron.write()
+       
+
+    def replied(self):
+
+        if self.first_failure is not None:
+
+            self.first_failure = None
+            self.remove_ping_job()    
+
+
+    def remove_ping_job(self):
+
+        cron = CronTab()
+        for job in cron:
+            if job.comment == self.__job_comment():
+                cron.remove(job)
+                cron.write()
+        
+
+    def is_active(self):
+
+        return self.first_failure is not None
 
 
 class AddressBook:
@@ -55,13 +117,14 @@ class AddressBook:
     list: initial list of contacts
     receiver_notify_interval: notify interval for incoming messages
     """
-    def __init__(self, self_contact: Contact, contacts: list = [], receiver_notify_interval=1):
+    def __init__(self, self_contact: Contact, contacts: list = [], receiver_notify_interval=1, contact_restore_timeout=3600):
         
         self.receiver = MessageReceiver(self_contact.port, notify_interval=receiver_notify_interval)
         
         self.contacts = contacts.copy()
         self.receiver.register_consumer(self)
         self.self_contact = self_contact
+        self.contact_restore_timeout = contact_restore_timeout
 
 
     def parse_message(self, raw_message):
@@ -73,19 +136,6 @@ class AddressBook:
         data = raw_message['data']
 
         return command, data
-
-
-    def __generate_add_contact_message(self, contact: Contact):
-
-        """
-        Generates an "add-contact" message.
-        contact: add-contact message payload
-        """
-
-        return {
-            'command': 'add-contact',
-            'data': contact
-        }
 
 
     def __add_contact(self, contact: Contact):
@@ -136,12 +186,49 @@ class AddressBook:
         recipient: recipient node's contact
         message: message to send
         """
-        
+
         sender = MessageSender(recipient.host, recipient.port)
 
-        sender.send_message(message)
+        try:
 
+            sender.send_message(message)
+
+        except ConnectionRefusedError:
+            
+            recipient.did_not_reply()
     
+
+    def __delete_contact(self, contact: Contact):
+        """
+        Deletes a contact.
+        """
+        for known_contact in self.contacts:
+            
+            if known_contact.id == contact.id:
+
+                self.contacts.remove(known_contact)
+
+                known_contact.remove_ping_job()
+
+                return
+
+
+    def __ping_successful(self, contact: Contact):
+
+        contact.replied()
+
+
+    def __ping_unsuccessful(self, contact: Contact):
+
+        for known_contact in self.contacts:
+
+            if contact.id == known_contact.id:
+                
+                if now() - known_contact.first_failure > self.contact_restore_timeout:
+                    
+                    self.__delete_contact(known_contact)
+    
+
     def notify(self, message):
         """
         Handles incoming messages.
@@ -152,6 +239,14 @@ class AddressBook:
         if command == 'add-contact':
 
             self.__add_contact(data)
+
+        elif command == 'ping-unsuccessful':
+            
+            self.__ping_unsuccessful(data)
+
+        elif command == 'ping-successful':
+
+            self.__ping_successful(data)
 
 
     def create_new_distributed_contact(self, contact: Contact):
@@ -168,6 +263,43 @@ class AddressBook:
 
             self.__send_message_to_contact(known_contact, message)
             
+
+
+def __generate_add_contact_message(contact: Contact):
+    """
+    Generates an "add-contact" message.
+    contact: add-contact message payload
+    """
+    
+    return {
+        'command': 'add-contact',
+        'data': contact
+    }
+
+
+def __generate_ping_unsuccessful_message(contact: Contact):
+    """
+    Generates an "ping-unsuccessful" message.
+    contact: ping-unsuccessful message payload
+    """
+    
+    return {
+        'command': 'ping-unsuccessful',
+        'data': contact
+    }
+
+
+def __generate_ping_successful_message(contact: Contact):
+    """
+    Generates an "ping-successful" message.
+    contact: ping-successful message payload
+    """
+    
+    return {
+        'command': 'ping-successful',
+        'data': contact
+    }
+
 
 def __demo():
 
@@ -218,6 +350,86 @@ def __demo():
         time.sleep(5)
 
 
+def __ping_host(host: str, port: int, ab_port: int, contact_id: str):
+
+    ping_sender = MessageSender(host, port)
+    ab_sender = MessageSender('127.0.0.1', ab_port)
+
+    pinged_contact = Contact(contact, host, port)
+
+    try:
+        ping_sender.send_message("Helo")
+
+    except:
+
+        print("Host down")
+        ab_sender.send_message(__generate_ping_unsuccessful_message(pinged_contact))
+        return
+
+    ab_sender.send_message(__generate_ping_successful_message(pinged_contact))
+
+    
+
 if __name__ == '__main__':
 
-    __demo()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-d", "--demo", action='store_true', help="Run demo")
+
+    parser.add_argument("-p", "--ping-node", action='store_true')
+    parser.add_argument("--host")
+    parser.add_argument("--port", type=int)
+    parser.add_argument("--ab-port", type=int)
+    parser.add_argument("--test", action='store_true')
+    parser.add_argument("--test-receiver", action='store_true')
+    parser.add_argument("--contact-id")
+    
+    args = parser.parse_args()
+
+    if args.demo:
+
+        __demo()
+
+    elif args.test:
+
+        contact = Contact("", "127.0.0.1", 8000)
+        contact.did_not_reply(minutes_interval=1)
+
+
+    elif args.test_receiver:
+
+        receiver = MessageReceiver(8000)
+
+        class MyConsumer(MessageConsumer):
+
+            def notify(self, message):
+
+                print(message)
+
+        receiver.register_consumer(MyConsumer())
+
+        print("Listening")
+
+    elif args.ping_node:
+
+        if args.host is None:
+
+            print("No host was specified")
+            exit(1)
+
+        if args.port is None:
+
+            print("No port was specified")
+            exit(1)
+            
+        if args.ab_port is None:
+
+            print("No self port was specified")
+            exit(1)
+
+        if args.contact_id is None:
+
+            print("No contact id specified")
+            exit(1)
+
+        __ping_host(args.host, args.port, args.ab_port, args.contact_id)
