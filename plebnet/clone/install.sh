88c8fd227ce637a284175b772d6ee883cab71537
mmito <45098081+mmito@users.noreply.github.com>
2020-05-29 18:25:28 +0200

    Add -y to python3-distutils install.

--- plebnet/clone/install.sh
+++ plebnet/clone/install.sh
@@ -38,7 +38,7 @@ apt-get update
 # && apt-get -y upgrade
 
 apt-get install -y python3
-apt-get install python3-distutils
+apt-get install -y python3-distutils
 
 # Reinstall pip
 apt-get remove --purge -y python-pip
